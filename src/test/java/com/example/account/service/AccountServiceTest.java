0e9c2503b168f4509794712db5124b9e71cd4b0c
geun02k <geun02k@gmail.com>
2024-11-29 04:49:43 +0900

    Mocking - 테스트에 필요한 클래스들의 의존성 문제해결 -> 해당 기능이 맡은 역할만 테스트가능

--- src/test/java/com/example/account/service/AccountServiceTest.java
+++ src/test/java/com/example/account/service/AccountServiceTest.java
@@ -3,12 +3,18 @@ package com.example.account.service;
 
 import com.example.account.domain.Account;
 import com.example.account.domain.AccountStatus;
-import org.junit.jupiter.api.BeforeEach;
+import com.example.account.repository.AccountRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 
 // @SpringBootTest
 // : org.springframework.boot:spring-boot-starter-test 에 포함된 기능
@@ -19,39 +25,65 @@ import static org.junit.jupiter.api.Assertions.*;
 // private AccountService accountService = new AccountService(new AccountRepository());와 같이 직성하지않고
 // @Autowired
 // private AccountService accountService;
-@SpringBootTest
+
+// @SpringBootTest -> @ExtendWith(MockitoExtension.class)
+// Junit을 그대로 이용해서는 Mockito 기능 사용불가.
+// Mockito 확장팩을 테스트 클래스에 달아줌.
+@ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
-    @Autowired
+    // accountService는 AccountRepository에 의존하고있다.
+    // AccountRepository를 가짜로 생성해 AccountService에 의존성을 추가해준다.
+
+    // @Mock
+    // : Mockito 라이브러리에서 제공하는 어노테이션
+    // : AccountRepository를 가짜로 생성해 accountRepository에 담아준다.
+    //   (injection, 의존성주입과 비슷하게 생성해준다.)
+    @Mock
+    private AccountRepository accountRepository;
+
+    // Mock으로 생성해준 accountRepository를 accountService에 inject.
+    @InjectMocks
     private AccountService accountService;
 
-    // 전체 테스트 메서드 수행 전 수행 (계좌생성)
-    @BeforeEach
-    void init() {
-        accountService.createAccount();
-    }
-
     // main함수가 아니지만 실행가능한 이유 : Junit 프레임워크가 대신 실행해주기 떄문.
+    /**
+        Mock을 사용해 테스트하면
+        동일 테스트코드를 함께 실행해도 하나는 성공하고 하나는 실패하는 문제 해결가능.
+        DB에 데이터가 바뀌든 의존하고 있는 repository의 로직이 변경되든 관계없이
+        내가 맡은 역할만 테스트가능.
+     */
     @Test
     void testGetAccount() {
+        // findById() 메서드 반환값은 Optional이므로 Optional타입의 Account를 반환해준다.
+        // given
+        given(accountRepository.findById(anyLong()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountNumber("65789")
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .build()));
+
         // when
-        Account account = accountService.getAccount(1L);
+        Account account = accountService.getAccount(4555L);
 
         // then
-        assertEquals("40000", account.getAccountNumber());
-        assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
+        assertEquals("65789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
     }
 
-    // @BeforeEach가 각 메서드 수행전에 동작하기 때문에 testGetAccount2()를 수행하는 경우 성공하고,
-    // testGetAccount() 메서드의 getAccount(2L)로 했을 때는 실패한다.
-    // 동일 테스트코드인데 어떤 것은 성공하고 어떤 것은 실패하게 되는 것이다.
-    // 이를 해결하기위해 Mocking을 이용한다.
-//    @Test
-//    void testGetAccount2() {
-//        // when
-//        Account account = accountService.getAccount(2L);
-//
-//        // then
-//        assertEquals("40000", account.getAccountNumber());
-//        assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
-//    }
+    @Test
+    void testGetAccount2() {
+        given(accountRepository.findById(anyLong()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountNumber("65789")
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .build()));
+
+        // when
+        Account account = accountService.getAccount(4555L);
+
+        // then
+        assertEquals("65789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
+    }
+
 }
