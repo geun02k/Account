546857941d338eccbd7731016c903ab9cfe9b395
geun02k <geun02k@gmail.com>
2024-12-04 04:43:54 +0900

    계좌생성API - 계좌생성api controller 성공 테스트케이스 작성

--- src/test/java/com/example/account/controller/AccountControllerTest.java
+++ src/test/java/com/example/account/controller/AccountControllerTest.java
@@ -2,18 +2,26 @@ package com.example.account.controller;
 
 import com.example.account.domain.Account;
 import com.example.account.domain.AccountStatus;
+import com.example.account.dto.AccountDto;
+import com.example.account.dto.CreateAccount;
 import com.example.account.service.AccountService;
 import com.example.account.service.RedisTestService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -36,6 +44,43 @@ class AccountControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper; // jackson을 이용해 object <-> 문자열로 변환
+
+    @Test
+    void successCreateAccount() throws Exception {
+        // given
+        // createAccount mocking (createAccount메서드의 실행결과 임의 셋팅)
+        given(accountService.createAccount(anyLong(), anyLong()))
+                .willReturn(AccountDto.builder()
+                        .userId(1L)
+                        .accountNumber("1234567890")
+                        .registeredAt(LocalDateTime.now())
+                        .unRegisteredAt(LocalDateTime.now())
+                        .build());
+        // when
+
+        // then
+        // objectMapper.writeValueAsString(new CreateAccount.Request(1L, 100L)
+        // -> 아래 형태의 JSON 문자열 생성, content에 그냥 json형태의 문자열을 삽입해도 결과동일.
+        // {
+        //  "userId": 1,
+        //  "initialBalance": 10000
+        // }
+        // 그런데 요청값이 실제 응답값에 영향을 주진 않음. (요청 : userId=3333L, accountNumber=1111L)
+        // given에서 작성한 결과값을 응답으로 리턴함. (응답 : userId=1, accountNumber=1234567890)
+        mockMvc.perform(post("/account") // /account url로 post 전송
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(
+                        new CreateAccount.Request(3333L, 1111L)))) // controller에 들어갈 요청 (CreateAccount.Request 정보)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(1))
+                .andExpect(jsonPath("$.accountNumber").value("1234567890"))
+                .andDo(print());
+
+    }
+
 
     // Service쪽 테스트할 때는 when에서 요청한 다음 결과를 받아 확인.
     // controller는 결과가 객체로 만들어지는 게 아닌 http 프로토콜의 응답형식으로 온다.
